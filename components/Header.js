eafb5e260502673e83f4dd38a40029ed991e49c2
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-10-02 09:25:37 -0600

    Finished task 1

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,29 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const parentDiv = document.querySelector('.header-container')
+function Header() {
+    //declaring vars
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+    
+    //adding classes
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
 
-function Header() {}
+    //adding text Content
+    date.textContent = 'March 28, 2020';
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    parentDiv.appendChild(headerDiv);
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(title);
+    headerDiv.appendChild(temp);
+    
+    return headerDiv
+}
+Header();
